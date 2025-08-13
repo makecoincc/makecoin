ab9141062d8515cf932ce4de669f02fb080e468b
Alberto Chen <xchao0213@gmail.com>
2025-08-13 20:54:10 +0800

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+import { Button } from '@heroui/react';
+
+export default function Home() {
+  return (
+    <div className="h-screen">
+      <Button color="primary">button</Button>
+    </div>
+  );
+}
