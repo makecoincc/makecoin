dac4917b4cf341c10f2d46398fa88da39517c933
Alberto Chen <xchao@Albertos-Mac-mini.local>
2025-10-20 06:24:52 +0800

    chain tools

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,9 +1,11 @@
 'use client'
-import Hero from '@/components/Hero'
+import Hero from '@/components/Hero';
+import CoinTools from '@/components/CoinTools';
 export default function Home() {
   return (
     <>
       <Hero />
+      <CoinTools />
     </>
   );
 }
