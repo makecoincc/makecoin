ccbb4dbf6786264684f7157573e33752350e252e
Alberto Chen <xchao0213@gmail.com>
2025-11-21 14:29:27 +0800

    mobile wallet

--- app/provider/solana-provider.tsx
+++ app/provider/solana-provider.tsx
@@ -6,13 +6,20 @@ import {
 } from "@solana/wallet-adapter-react";
 import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
 import "@solana/wallet-adapter-react-ui/styles.css";
+import {
+  PhantomWalletAdapter,
+} from '@solana/wallet-adapter-wallets';
 interface SolanaProviderProps {
   children: ReactNode;
 }
 
+const wallets = [
+  new PhantomWalletAdapter(),
+];
+
 export const SolanaProvider: FC<SolanaProviderProps> = ({ children }) => {
   return (
-    <WalletProvider wallets={[]} autoConnect>
+    <WalletProvider wallets={wallets} autoConnect>
       <WalletModalProvider>
         {children}
       </WalletModalProvider>
